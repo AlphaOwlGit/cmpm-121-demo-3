19029a8bcad34671071222561815492e59487a08
AlphaOwl <98154867+AlphaOwlGit@users.noreply.github.com>
2024-11-04 01:12:00 -0800

    Initial test commit (L16 Dialog)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 // todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const gameName = "Click The Button";
+
+document.title = gameName;
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "Click Me!";
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
+app.append(button);
